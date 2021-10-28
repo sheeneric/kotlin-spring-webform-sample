7aa93e2df0fa607ca109cdcb8efe5920cab5dffb
eric <esheen11@gmail.com>
2021-10-28 18:39:58 -0500

    spring initialize

---
+++ src/main/kotlin/com/spring/webform/WebformApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.webform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebformApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebformApplication>(*args)
+}
